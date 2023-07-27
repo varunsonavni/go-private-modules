81d3eaa1ffde5d3c6ad1334787c0550df7486df7
Varun Sonavni <varun.sonavni@converj.com>
2023-07-27 18:25:47 +0530

    update folder structure

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/varunsonavni/go-private-modules/src/helm"
 )
 
 func main() {
 	// fmt.Println("HTTP request server listening on port 8080..")
 	log.Println("HTTP request server listening on port 8080..")
-	http.HandleFunc("/api", HandleAPIRequest)
+	http.HandleFunc("/api", helm.HandleAPIRequest)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
