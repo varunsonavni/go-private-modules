95d7a7a63d0611e2040b63e0965ff9520077dba8
Varun Sonavni <varun.sonavni@converj.com>
2023-07-27 16:58:25 +0530

    added main file

---
+++ main.go
@@ -0,0 +1,13 @@
+package goprivatemodules
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// fmt.Println("HTTP request server listening on port 8080..")
+	log.Println("HTTP request server listening on port 8080..")
+	http.HandleFunc("/api", HandleAPIRequest)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
